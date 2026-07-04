651b6540daabf3711ce67a3327de9728411bf328
Samiran2004 <samiran.samanta.dev@gmail.com>
2026-07-04 20:27:43 +0530

    refactor: update import path for geoApi in provider-location page

--- src/app/provider-location/page.tsx
+++ src/app/provider-location/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { updateProviderLocation, getCurrentPosition } from '@/lib/geoApi';
+import { updateProviderLocation, getCurrentPosition } from '../../lib/geoApi';
 
 interface FormState {
   latitude: string;
